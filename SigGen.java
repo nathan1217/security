c279494d8004a114fa55e2970adb35f7cd703562
nathan <nathan@qq.com>
2022-10-02 16:35:59 +0800

    sig

--- SigGen.java
+++ SigGen.java
@@ -15,42 +15,35 @@ public class SigGen {
                 KeyStore ks = KeyStore.getInstance("PKCS12");
                 FileInputStream ksfis = new FileInputStream("demo-cert.pkcs12");
                 BufferedInputStream ksbufin = new BufferedInputStream(ksfis);
-
                 // open keystore and get private key
                 // alias is 'signLeal', kpasswd/spasswd is 'vagrant'
                 ks.load(ksbufin, "1234qwer".toCharArray());
                 PrivateKey priv = (PrivateKey) ks.getKey("user", "1234qwer".toCharArray());
 
                 /* Create a Signature object and initialize it with the private key */
-                Signature dsa = Signature.getInstance("SHA256withRSA", "SunJSSE");
-
-                dsa.initSign(priv);
+                Signature rsaSig = Signature.getInstance("SHA256withRSA");
+                rsaSig.initSign(priv);
                 /* Update and sign the data */
-
                 FileInputStream fis = new FileInputStream(args[0]);
                 BufferedInputStream bufin = new BufferedInputStream(fis);
                 byte[] buffer = new byte[1024];
                 int len;
                 while (bufin.available() != 0) {
                     len = bufin.read(buffer);
-                    dsa.update(buffer, 0, len);
+                    rsaSig.update(buffer, 0, len);
                 }
-                ;
 
                 bufin.close();
-
                 /*
                  * Now that all the data to be signed has been read in,
                  * generate a signature for it
                  */
-                byte[] realSig = dsa.sign();
+                byte[] realSig = rsaSig.sign();
 
                 /* Save the signature in a file */
                 FileOutputStream sigfos = new FileOutputStream("sig");
                 sigfos.write(realSig);
-
                 sigfos.close();
-
                 /*
                  * public key file can export from keystore use keytool:
                  * $ keytool -export -keystore examplestanstore -alias signLegal -file
--- SigVer.java
+++ SigVer.java
@@ -13,31 +13,28 @@ public class SigVer {
                 java.security.cert.CertificateFactory cf = java.security.cert.CertificateFactory.getInstance("X.509");
                 java.security.cert.Certificate cert = cf.generateCertificate(certfis);
                 PublicKey pubKey = cert.getPublicKey();
-
-                /* input the signature bytes */
-                FileInputStream sigfis = new FileInputStream(args[1]);
-                byte[] sigToVerify = new byte[sigfis.available()];
-                sigfis.read(sigToVerify);
-
-                sigfis.close();
-
                 /* create a Signature object and initialize it with the public key */
-                Signature sig = Signature.getInstance("SHA256withRSA", "SunJSSE");
-                sig.initVerify(pubKey);
+                Signature rsaSig = Signature.getInstance("SHA256withRSA");
+                rsaSig.initVerify(pubKey);
 
                 /* Update and verify the data */
-
                 FileInputStream datafis = new FileInputStream(args[2]);
                 BufferedInputStream bufin = new BufferedInputStream(datafis);
                 byte[] buffer = new byte[1024];
                 int len;
                 while (bufin.available() != 0) {
                     len = bufin.read(buffer);
-                    sig.update(buffer, 0, len);
+                    rsaSig.update(buffer, 0, len);
                 }
-
                 bufin.close();
-                boolean verifies = sig.verify(sigToVerify);
+
+                /* input the signature bytes */
+                FileInputStream sigfis = new FileInputStream(args[1]);
+                byte[] sigToVerify = new byte[sigfis.available()];
+                sigfis.read(sigToVerify);
+                sigfis.close();
+
+                boolean verifies = rsaSig.verify(sigToVerify);
                 System.out.println("signature verifies: " + verifies);
             } catch (Exception e) {
                 System.err.println("Caught exception " + e.toString());
